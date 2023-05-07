0f4729b9e71e73685140f3bb3f26d92965ab81ec
SongYalong <494283111@qq.com>
2023-05-07 21:26:53 +0800

    main

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,4 +32,3 @@ source_suffix = ['.rst', '.md']
 import sphinx_rtd_theme
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-html_static_path = ['_static']
